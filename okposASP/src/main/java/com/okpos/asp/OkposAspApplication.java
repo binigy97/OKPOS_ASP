cb02d254ad57a507bca19cff4e09ff92ab9afa55
binigy97 <75118895+binigy97@users.noreply.github.com>
2022-03-09 22:07:06 +0900

    1.0.0
    Create a project

---
+++ okposASP/src/main/java/com/okpos/asp/OkposAspApplication.java
@@ -0,0 +1,13 @@
+package com.okpos.asp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OkposAspApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OkposAspApplication.class, args);
+	}
+
+}
---
+++ okposASP/src/test/java/com/okpos/asp/OkposAspApplicationTests.java
@@ -0,0 +1,13 @@
+package com.okpos.asp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OkposAspApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
